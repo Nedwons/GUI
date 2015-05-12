dc62a2d849b9ec1cb7b27295bc03872360c960be
Vadim Goshko <vadim_193@mail.ru>
2015-05-12 09:55:48 +0300

    Попытка слияния

--- JFilechooser/src/jfilechooser/Controller.java
+++ JFilechooser/src/jfilechooser/Controller.java
@@ -98,24 +98,24 @@ public class Controller {
                         Integer[] mistakeAndProgress = new Integer[2];
                         mistakeAndProgress[1] = 0;
                         mistakeAndProgress[0] = 0;
-                        int blockSize = 1572;
+                        int blockSize = 128;  // размер блока в байтах
                         //int counter = 0; // счетчик битых байтов
                         int bytes = 0;  // количество считанных байт
 
                         for(int z = 0; (c = in.read()) != -1; bytes++, publish(mistakeAndProgress)) {    // считывание файла. (условие завершения цикла - достижение конца файла)
                             mistakeAndProgress[1]++; // обновление прогрессбара
                             z++;
-                            int result = mask(0.9, c); // result - 8 бит файла с внесенными ошибками
+                            int result = mask(probability, c); // result - 8 бит файла с внесенными ошибками
                             out.write(result); // запись на диск                         
                             if( (z == blockSize) ||(view.getFileSize() == bytes - 1) ) { // при прочтении блока из заданного числа байт проверить
                                                        //его на ошибки и обнулить локальный счетчик ошибок (то же при достижении конца файла,
                                                         // когда последний блок не полный )
                                 if ( getByteErrorFlag() ){
-                                    System.out.println("Ошибка блока!");
+                                    //System.out.println("Ошибка блока!");
                                     setBlockError( 1 );
                                     setBlockErrorFlag( false ); // обнулить флаг ошибки в блоке
                                 }
-                                System.out.println("Отсчет блока");
+                                //System.out.println("Отсчет блока");
                                 z = 0; // сбросить счетчик байт в блоке
                             }
                         }
@@ -160,34 +160,35 @@ public class Controller {
         worker.execute();
     }
     
-    // метод складывает по модулю 2 8 битную маску ошибок в битах с участком файла. 
+    // метод складывает по модулю 2 8 битную маску ошибок в битах с участком файла. Т.е. генерирует т.н. поток ошибок
     // входные данные: probaility - вероятность искажения бита; data - 8 битный участок файла.
     // выходные - 8 участок файла с искажениями
+    // также метод ведет подсчет ошибок по битам и байтам, выставляет флаг для подсчета ошибок по блокам.
     public int mask(double probability, int data){
         int shiftVariable = 1;
         int tempMask = 0;
         int tempBitError = 0; // обнуление счетчика ошибочных бит 
         Random randomNumberGenerator = new Random();
         for(int i = 0; i != 8;  shiftVariable = shiftVariable << 1,i++) {
-            System.out.println("Цикл "+i);
+            //System.out.println("Цикл "+i);
             if(randomNumberGenerator.nextDouble() > probability){
                 if( i == 0 ){ //если первая итерация 
-                    System.out.format("\n i = %d Накладываем маску %h ",i, tempMask );
+                    //System.out.format("\n i = %d Накладываем маску %h ",i, tempMask );
                     tempMask = tempMask | shiftVariable; // побитовое сложение нулевой маски со сдвинутой
                     tempBitError++; // изменяем количество битовых ошибок
-                    System.out.format("на %h . получаем %h\n", shiftVariable,tempMask );
+                   // System.out.format("на %h . получаем %h\n", shiftVariable,tempMask );
                 } 
                 else { // если цикл не первый
-                    System.out.format("i = %d Накладываем маску %h ",i, tempMask );
+                    //System.out.format("i = %d Накладываем маску %h ",i, tempMask );
           
                     tempMask = tempMask | shiftVariable;
                     tempBitError++;
                     
-                    System.out.format("на %h . получаем %h\n", shiftVariable,tempMask );
+                    //System.out.format("на %h . получаем %h\n", shiftVariable,tempMask );
                 }
             }
             else {
-                System.out.println("Без ошибок ("+i+")");
+                //System.out.println("Без ошибок ("+i+")");
             }
         }
         setBitError( tempBitError ); // обновление количества искаженных бит
@@ -195,10 +196,10 @@ public class Controller {
                 setBlockErrorFlag( true ); //есть ошибка в байте. Используется в подсчетче ошибочных блоков.
                 setByteError( 1 ); // обновление количества искаженных байт
         }
-        System.out.format("\n\n\n финальная маска = %h\n", tempMask );
-        System.out.format("\n\n\n файл = %h\n", data);
-        System.out.format("\n\n\n файлова маска = %h\n", data ^ tempMask);
-        System.out.println("-----------------------------------------------");
+//        System.out.format("\n\n\n финальная маска = %h\n", tempMask );
+//        System.out.format("\n\n\n файл = %h\n", data);
+//        System.out.format("\n\n\n файлова маска = %h\n", data ^ tempMask);
+//        System.out.println("-----------------------------------------------");
         return data ^ tempMask;
     } 
 }
